9a2a905843ef4aab4e38f6007773ed4e6e14b2d4
Sean R Milligan <seanrmilligan@gmail.com>
2017-06-12 01:00:09 -0400

    --list-remotes implemented, new help menu

--- git-utils.py
+++ git-utils.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import subprocess
 import sys
 
-descr = 'This tool takes a path to a directory containing git repositories, and returns only the repositories which have uncommitted changes.'
-usage = 'usage: {:s} DIR'
-usageargs = ['\tDIR   The directory to recursively search for git repos.']
+descr = '    This tool recursively searches for git repositories within a directory,\n    returning information based on the selected operation.'
+usageargs = [
+	'    --list-remotes',
+	'        List the remote repo URL(s) for each git repo in DIR.',
+	'    --list-uncommited',
+	'        List the repos for which there are uncommitted changes in DIR.',
+	'    DIR',
+	'        The directory to recursively search for git repositories.',
+	''
+]
+usageopts = ['    {:s} --list-remotes DIR', '    {:s} --list-uncommitted DIR']
 error_str = '[Error]'
 invalid_argc_str = 'Invalid number of arguments.'
+invalid_param_str = 'Parameter: {:s} is not a valid option.'
 invalid_path_str = 'The specified path ({:s}) does not exist.'
 info_str = '[Info]'
 resources_str = 'For more information, see {:s} --help'
 
 debug = True
 
-def check(path):
+def list_remotes(path):
+	os.chdir(path)
+
+	if repo(path):
+		print('{:s}: {:s}'.format(path, remote(path))) 
+	else:
+		for dir in subdirs(path):
+			list_remotes(dir)
+
+def list_uncommitted(path):
 	os.chdir(path)
 
 	if repo(path):
@@ -23,7 +42,7 @@ def check(path):
 			print(path)
 	else:
 		for dir in subdirs(path):
-			check(dir)
+			list_uncommitted(dir)
 
 def repo(path):
 	success_code = 0
@@ -32,6 +51,16 @@ def repo(path):
 		process = subprocess.run(["git", "status"], stdout=null_file, stderr=null_file)
 
 	return process.returncode is success_code
+
+def remote(path):
+	pattern = re.compile("git@(.*)\.git")
+
+	with open('/dev/null', 'w') as null_file:
+		process = subprocess.run(["git", "remote", "-v"], stdout=subprocess.PIPE, stderr=null_file)
+
+	matches = re.search(pattern, str(process.stdout, 'utf-8'))
+	
+	return matches.group(0) if matches else "no remote"
 
 def up_to_date(path):
 	with open('/dev/null', 'w') as null_file:
@@ -53,9 +82,17 @@ def inform(info):
 		print('{:s} {:s}'.format(info_str, info))
 
 def helpmenu(progname):
+	print("NAME")
+	print(progname)
+	print()
+	print("DESCRIPTION")
 	print(descr)
 	print()
-	print(usage.format(progname))
+	print("USAGE")
+	for line in usageopts:
+		print(line.format(progname))
+	print()
+	print("PARAMETERS")
 	for line in usageargs:
 		print(line)
 
@@ -65,9 +102,15 @@ if __name__ == '__main__':
 
 	if argc is 1 or '--help' in sys.argv:
 		helpmenu(progname)
-	elif argc is 2:
-		abs_path = os.path.abspath(sys.argv[1])
-		check(abs_path)
+	elif argc is 3:
+		operation = sys.argv[1]
+		path = os.path.abspath(sys.argv[2])
+		if operation == "--list-uncommitted":
+			list_uncommitted(path)
+		elif operation == "--list-remotes":
+			list_remotes(path)
+		else:
+			warn(invalid_param_str.format(operation), progname)
 	else:
 		warn(invalid_argc_str, progname)
 		
